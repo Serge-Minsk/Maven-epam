e9f0051da398ead9202219b1b52cce2234f6aacd
“Dirman” <s.v.dirman@gmail.com>
2016-03-12 05:33:58 +0300

    java code modyfied

--- src/main/java/com/test/Project.java
+++ src/main/java/com/test/Project.java
@@ -8,6 +8,11 @@ class Project {
   public static void test() {
     System.out.println("test");
   }
+
+public static void test() {
+    System.out.println("hello");
+  }
+
   
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
